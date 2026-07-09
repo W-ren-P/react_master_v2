dd35c238311e4414fb70bca730a2f6150ff6c712
WarrenPegg <warren.pegg@hotmail.co.uk>
2026-07-09 13:20:04 +0100

    Got webpage to load

--- cp-bootcamp/src/App.jsx
+++ cp-bootcamp/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import bundImg from "./assets/bundesliga_logo.jpg";
 
 import "./App.css";
+// import Header from "./Header";
 
 function App() {
   const [count, setCount] = useState(0);
